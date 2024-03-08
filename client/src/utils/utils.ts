0c0f435964ddaa9ec328844844ba248b88201faa
Dorian <98755570+Doori4N@users.noreply.github.com>
2024-03-09 00:30:53 +0100

    added interpolation

--- client/src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Linear interpolation between two numbers
- * @param start
- * @param end
- * @param t
- */
-export const lerp = (start: number, end: number, t: number): number => {
-    return start + (end - start) * t;
-}
